7c060fe28510b8924a12bb26cfca753f9930de39
Guilherme Caixeta <guilherme.lpcaixeta@gmail.com>
2019-07-26 17:41:34 +0100

    Update tests

--- src/Generic.Repository.Test/Repository/BaseRepositoryAsyncQueryTest.cs
+++ src/Generic.Repository.Test/Repository/BaseRepositoryAsyncQueryTest.cs
@@ -19,93 +19,89 @@ namespace Generic.Repository.Test.Repository
         protected int count3;
 
         public BaseRepositoryAsyncQueryTest()
-        {
-            DeleteBase();
-        }
+        { }
 
         protected abstract IEnumerable<TValue> GetListSimpleObject();
 
         [Test]
         public async Task Get_All_DataAsync()
         {
-            var data = GetListSimpleObject();
-            SaveList(data);
+            SetEnvironment();
             var list = await _repository.GetAllAsync(true);
 
             Assert.IsNotNull(list);
             Assert.AreEqual(count, list.Count);
-            DeleteBase();
         }
 
         [Test]
         public async Task Get_All_By_DataAsync()
         {
-            var data = GetListSimpleObject();
-            SaveList(data);
-            var list = await _repository.GetAllByAsync(ExpressionGeneric() ,true);
+            SetEnvironment();
+            var list = await _repository.GetAllByAsync(ExpressionGeneric(), true);
             var result = list.Count;
             Assert.IsNotNull(list);
             Assert.AreEqual(count2, result);
-            DeleteBase();
         }
 
         [Test]
         public async Task Filter_All_DataAsync()
         {
-            var data = GetListSimpleObject();
-            SaveList(data);
+            SetEnvironment();
+
             var list = await _repository.FilterAllAsync(GetFilter(), true);
 
             Assert.IsNotNull(list);
             Assert.AreEqual(count3, list.Count);
-            DeleteBase();
         }
 
         [Test]
         public async Task First_DataAsync()
         {
-            var data = GetListSimpleObject();
-            SaveList(data);
+            SetEnvironment();
+
             var value = await _repository.GetFirstByAsync(ExpressionGeneric(), true);
 
             Assert.IsNotNull(value);
-            DeleteBase();
         }
 
         [Test]
         public async Task Page_All_DataAsync()
         {
-            var data = GetListSimpleObject();
-            SaveList(data);
+            SetEnvironment();
+
             var page = await _repository.GetPageAsync(GetPageConfig(), true);
 
             Assert.IsNotNull(page);
-            Assert.AreEqual(page.TotalElements, count2);
-            DeleteBase();
+            Assert.AreEqual(count2, page.TotalElements);
         }
 
         [Test]
         public async Task Page_All_Expression_DataAsync()
         {
-            var data = GetListSimpleObject();
-            SaveList(data);
+            SetEnvironment();
+
             var page = await _repository.GetPageAsync(GetPageConfig(), ExpressionGeneric(), true);
 
             Assert.IsNotNull(page);
-            Assert.AreEqual(page.TotalElements, count2);
-            DeleteBase();
+            Assert.AreEqual(count2, page.TotalElements);
         }
 
         [Test]
         public async Task Page_All_Filter_DataAsync()
         {
-            var data = GetListSimpleObject();
-            SaveList(data);
+            SetEnvironment();
+
             var page = await _repository.GetPageAsync(GetPageConfig(), GetFilter(), true);
 
             Assert.IsNotNull(page);
-            Assert.AreEqual(page.TotalElements, count3);
+            Assert.AreEqual(count3, page.TotalElements);
+        }
+
+        private void SetEnvironment()
+        {
             DeleteBase();
+            var data = GetListSimpleObject();
+            SaveList(data);
         }
 
         internal abstract IPageConfig GetPageConfig();
